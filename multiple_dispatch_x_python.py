28a5d90f9139ce8c0b353acfa0d3e1f03e804270
Mustafa2 <Mustafauyar91@gmail.com>
2024-04-18 15:21:03 +0300

    MD

--- multiple_dispatch_x_python.py
+++ multiple_dispatch_x_python.py
@@ -1,1 +1,20 @@
-a = 5
+def f(x:int):
+    return "bu bir tam sayi"
+
+def f(x:str):
+    return "bu bir string"
+
+def my_func(x):
+    return f(x)
+
+my_func(5) # burda en son yazdığımız f fonksiyonu üzerine yazıldğı için (overloading) her zaman ne girilirse girilsi bu bir string dir yazar
+
+# julia daki multiple dispatch in pythondaki overloading daki farkı bu örnekteki gibidir
+
+# aynı isme sahip değişken varsa en son yazılanı kabul eder ve diğerlerini yok sayar
+
+my_func("mustafa")
+
+
+        
+        
